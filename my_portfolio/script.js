9f5308b9675ddbb977a19a0f553d3f254f28f884
Urshitha <urshitha23@gmail.com>
2025-05-28 09:23:27 -0500

    dynamic portfolio update

--- my_portfolio/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDarkMode(button) {
-  document.body.classList.toggle('dark-mode');
-  if (document.body.classList.contains('dark-mode')) {
-    button.textContent = '☀️';
-  } else {
-    button.textContent = '🌙';
-  }
-}
